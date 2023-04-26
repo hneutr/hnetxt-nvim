c7dcd4b95238b522d25a46a5942de74974119304
hneutr <hunter.wapman@gmail.com>
2023-04-25 23:48:19 -0600

    20230425

--- ftplugin/markdown/project.lua
+++ ftplugin/markdown/project.lua
@@ -1,1 +1,12 @@
-require("hnetxt-nvim.project").set()
+local Project = require("hnetxt-nvim.project")
+
+Project.set()
+
+vim.api.nvim_create_autocmd(
+    {"BufEnter"},
+    {
+        pattern="*.md",
+        group=vim.api.nvim_create_augroup('hnetxt_project_set', {clear = true}),
+        callback=Project.set
+    }
+)
--- lua/hnetxt-nvim/project/init.lua
+++ lua/hnetxt-nvim/project/init.lua
@@ -3,32 +3,6 @@ local Path = require("hneutil-nvim.path")
 
 local M = {}
 
--- function M.file.build(path)
---     local config = vim.fn.json_decode(vim.fn.readfile(path))
-
---     config['root'] = vim.fn.fnamemodify(path, ':h')
-
---     local mirrors = {}
---     for kind, kind_data in pairs(constants.mirror_defaults) do
---         for mirror, mirror_data in pairs(kind_data.mirrors) do
---             mirror_data = _G.default_args(vim.tbl_get(config, 'mirrors', mirror), mirror_data)
---             mirror_data.kind = kind
---             mirror_data.dir = Path.join(config['root'], kind_data.dir, mirror)
-
---             if not vim.tbl_get(mirror_data, "disable") then
---                 mirrors[mirror] = mirror_data
---             end
---         end
---     end
-
---     config['mirrors'] = mirrors
-
---     return config
--- end
-
---------------------------------------------------------------------------------
--- config
---------------------------------------------------------------------------------
 function M.set(start_path)
     start_path = start_path or Path.current_file()
 
@@ -36,24 +10,7 @@ function M.set(start_path)
         start_path = start_path.match
     end
 
-    local project = Project.from_path(path)
-
-    if project then
-        vim.b.hnetxt_project_root = project.root
-    end
+    vim.b.hnetxt_project_root = Project.root_from_path(path)
 end
-
--- function M.get()
---     return vim.tbl_get(vim.g.lex_configs or {}, vim.b.lex_config_path) or {}
--- end
-
-function M.push()
-    vim.fn.system("cd " .. vim.tbl_get(M.get(), 'root') or '.')
-    vim.fn.system("git add .")
-    vim.fn.system("git commit -m " .. vim.fn.strftime("%Y%m%d"))
-    vim.fn.system("git push")
-end
-
-
 
 return M
