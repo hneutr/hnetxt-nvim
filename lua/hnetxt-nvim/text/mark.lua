9490141b9e4fc906301f61f68565762af3f4b4f4
hneutr <hunter.wapman@gmail.com>
2023-04-27 02:54:27 -0600

    20230426

--- lua/hnetxt-nvim/text/mark.lua
+++ lua/hnetxt-nvim/text/mark.lua
@@ -3,14 +3,11 @@ local BufferLines = require("hneutil-nvim.buffer_lines")
 local Mark = require("hnetxt-lua.text.mark"):extend()
 
 function Mark.goto(label)
-    for i, line in ipairs(BufferLines.get()) do
-        if line:len() > 0 then
-            if Mark.str_is_a(line) and Mark.from_str(line).label == label then
-                vim.api.nvim_win_set_cursor(0, {i, 0})
-                vim.cmd("normal zz")
-                break
-            end
-        end
+    local line = Mark.find(label, BufferLines.get())
+
+    if line then
+        vim.api.nvim_win_set_cursor(0, {line, 0})
+        vim.cmd("normal zz")
     end
 end
 
