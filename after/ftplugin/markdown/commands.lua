620ba9e612e79c6be055fea5b7ba80e92ea88209
hneutr <hunter.wapman@gmail.com>
2023-06-06 19:11:57 -0600

    20230606

--- after/ftplugin/markdown/commands.lua
+++ after/ftplugin/markdown/commands.lua
@@ -1,10 +1,8 @@
-if vim.b.hnetxt_project_root then
-    local cmd = vim.api.nvim_buf_create_user_command
+local cmd = vim.api.nvim_buf_create_user_command
 
-    local Path = require("hn.path")
-    local Journal = require("htl.journal")
-    local Goals = require("htl.goals")
+local Path = require("hn.path")
+local Journal = require("htl.journal")
+local Goals = require("htl.goals.set")
 
-    cmd(0, "Journal", function() Path.open(Journal(vim.b.hnetxt_project_root)) end, {})
-    cmd(0, "Goals", function() Path.open(Goals()) end, {})
-end
+cmd(0, "Journal", function() Path.open(Journal(vim.b.hnetxt_project_root)) end, {})
+cmd(0, "Aim", function() Path.open(Goals.touch()) end, {})
